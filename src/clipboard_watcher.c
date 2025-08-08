07d1a88f7f025ce6b53992155147b0187d1526de
Специалист <213274794+specialistru@users.noreply.github.com>
2025-08-08 08:43:36 +0300

    Update clipboard_watcher.c

--- src/clipboard_watcher.c
+++ src/clipboard_watcher.c
@@ -80,16 +80,83 @@ void extract_filename_from_text(const char *text, const char *ext, char *out) {
 
 void generate_unique_filename(const char *base, const char *ext, char *out) {
     int index = 0;
+    size_t prefix_len = prefix_enabled ? strlen(prefix) : 0;
+    size_t base_len = strlen(base);
+    size_t ext_len = strlen(ext);
+
+    // Максимум для целочисленного индекса (до 10 цифр)
+    char index_str[12] = {0};
+    
     if (prefix_enabled) {
-        snprintf(out, MAX_FILENAME, "%s%s.%s", prefix, base, ext);
-    } else {
-        snprintf(out, MAX_FILENAME, "%s.%s", base, ext);
-    }
-    while (file_exists(out)) {
-        if (prefix_enabled) {
-            snprintf(out, MAX_FILENAME, "%s%s_%d.%s", prefix, base, index++, ext);
+        // Проверяем длину без индекса
+        if (prefix_len + base_len + 1 + ext_len + 1 > MAX_FILENAME - 1) {
+            // Обрезаем base, чтобы вместить в буфер
+            size_t allowed_base_len = MAX_FILENAME - 1 - prefix_len - 1 - ext_len - 1;
+            if (allowed_base_len > 0 && allowed_base_len < base_len) {
+                // Копируем часть base
+                char base_trunc[MAX_FILENAME];
+                strncpy(base_trunc, base, allowed_base_len);
+                base_trunc[allowed_base_len] = '\0';
+                snprintf(out, MAX_FILENAME, "%s%s.%s", prefix, base_trunc, ext);
+            } else {
+                snprintf(out, MAX_FILENAME, "%s.%s", prefix, ext);
+            }
         } else {
-            snprintf(out, MAX_FILENAME, "%s_%d.%s", base, index++, ext);
+            snprintf(out, MAX_FILENAME, "%s%s.%s", prefix, base, ext);
+        }
+    } else {
+        if (base_len + 1 + ext_len + 1 > MAX_FILENAME - 1) {
+            // Обрезаем base
+            size_t allowed_base_len = MAX_FILENAME - 1 - 1 - ext_len - 1;
+            if (allowed_base_len > 0 && allowed_base_len < base_len) {
+                char base_trunc[MAX_FILENAME];
+                strncpy(base_trunc, base, allowed_base_len);
+                base_trunc[allowed_base_len] = '\0';
+                snprintf(out, MAX_FILENAME, "%s.%s", base_trunc, ext);
+            } else {
+                snprintf(out, MAX_FILENAME, "file.%s", ext);
+            }
+        } else {
+            snprintf(out, MAX_FILENAME, "%s.%s", base, ext);
+        }
+    }
+
+    // Проверяем, существует ли файл
+    while (file_exists(out)) {
+        snprintf(index_str, sizeof(index_str), "_%d", index++);
+        size_t out_len = 0;
+
+        if (prefix_enabled) {
+            size_t len = prefix_len + base_len + strlen(index_str) + 1 + ext_len;
+            if (len > MAX_FILENAME - 1) {
+                // Обрезаем base для вмещения
+                size_t allowed_base_len = MAX_FILENAME - 1 - prefix_len - strlen(index_str) - 1 - ext_len;
+                if (allowed_base_len > 0 && allowed_base_len < base_len) {
+                    char base_trunc[MAX_FILENAME];
+                    strncpy(base_trunc, base, allowed_base_len);
+                    base_trunc[allowed_base_len] = '\0';
+                    snprintf(out, MAX_FILENAME, "%s%s%s.%s", prefix, base_trunc, index_str, ext);
+                } else {
+                    snprintf(out, MAX_FILENAME, "%s%s.%s", prefix, index_str, ext);
+                }
+            } else {
+                snprintf(out, MAX_FILENAME, "%s%s%s.%s", prefix, base, index_str, ext);
+            }
+        } else {
+            size_t len = base_len + strlen(index_str) + 1 + ext_len;
+            if (len > MAX_FILENAME - 1) {
+                size_t allowed_base_len = MAX_FILENAME - 1 - strlen(index_str) - 1 - ext_len;
+                if (allowed_base_len > 0 && allowed_base_len < base_len) {
+                    char base_trunc[MAX_FILENAME];
+                    strncpy(base_trunc, base, allowed_base_len);
+                    base_trunc[allowed_base_len] = '\0';
+                    snprintf(out, MAX_FILENAME, "%s%s.%s", base_trunc, index_str, ext);
+                } else {
+                    snprintf(out, MAX_FILENAME, "file%s.%s", index_str, ext);
+                }
+            } else {
+                snprintf(out, MAX_FILENAME, "%s%s.%s", base, index_str, ext);
+            }
         }
     }
 }
