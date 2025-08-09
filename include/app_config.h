0df6ca44cc24c919c9fe6cea2db55b76b3feb89d
Специалист <213274794+specialistru@users.noreply.github.com>
2025-08-09 09:49:55 +0300

    Update app_config.h

--- include/app_config.h
+++ include/app_config.h
@@ -1,1 +1,8 @@
+#pragma once
+// Блок: Конфигурация и автозапуск
+#include "app_state.h"
 
+#define CONFIG_FILE "clipboard_watcher.ini"
+
+void load_config(AppConfig* cfg);
+void enable_autostart_if_needed(const AppConfig* cfg);
